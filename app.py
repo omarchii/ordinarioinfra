2fa7128940e5a1c54b9d626485932f65505fd66c
omarchii <omarchi@192.168.68.57>
2024-12-03 14:59:52 -0600

    prueba3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Ruta para el endpoint GET
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'Hola Mundo'}), 200
+    return jsonify({'mensaje': 'Hola Mundoa'}), 200
 
 # Ejecución de la app
 if __name__ == '__main__':
